edec604223b87ad7f7e4f99682abff9d21ce3836
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2009-07-22 23:07:06 +0000

    added support for macro creation added reference to System.Web

--- trunk/d&d/TokenAssist/source/TokenGenerator.cs
+++ trunk/d&d/TokenAssist/source/TokenGenerator.cs
@@ -4,11 +4,20 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Web;
 
 namespace TokenAssist
 {
     public static class TokenGenerator
     {
+        public static string MacroCreationTemplate
+        {
+            get
+            {
+                return global::TokenAssist.Properties.Resources.MacroCreationTemplate;
+            }
+        }
+
         public static string NoWeaponTemplate
         {
             get
@@ -31,14 +40,15 @@ namespace TokenAssist
             {
                 foreach (Power power in character.Powers)
                 {
+                    string macro = null;
+
                     if (power.Weapons.Count == 0)
                     {
                         // this power does not involve weapons -- use the appropriate template
 
-                        string macro = NoWeaponTemplate;
-                        macro = macro.Replace("__POWER_NAME__", power.Name);
-                        macro = macro.Replace("__POWER_CARD__", (power.CompendiumEntry != null) ? power.CompendiumEntry : string.Empty);
-                        writer.WriteLine(macro);
+                        macro = NoWeaponTemplate;
+                        macro = macro.Replace(@"__POWER_NAME__", power.Name);
+                        macro = macro.Replace(@"__POWER_CARD__", (power.CompendiumEntry != null) ? power.CompendiumEntry : string.Empty);
                     }
                     else
                     {
@@ -57,18 +67,30 @@ namespace TokenAssist
                             maxDamageList += (maxDamageList.Length == 0) ? weapon.MaxDamage : string.Format(", {0}", weapon.MaxDamage);
                         }
 
-                        string macro = WeaponTemplate;
-                        macro = macro.Replace("__POWER_NAME__", power.Name);
-                        macro = macro.Replace("__WEAPON_LIST__", weaponList);
-                        macro = macro.Replace("__ATTACK_BONUS_LIST__", attackBonusList);
-                        macro = macro.Replace("__DAMAGE_LIST__", damageList);
-                        macro = macro.Replace("__MAX_DAMAGE_LIST__", maxDamageList);
-                        macro = macro.Replace("__MULTIPLE_TARGETS__", "0");
-                        macro = macro.Replace("__POWER_CARD__", (power.CompendiumEntry != null) ? power.CompendiumEntry : string.Empty);
-                        writer.WriteLine(macro);
-                    } 
+                        macro = WeaponTemplate;
+                        macro = macro.Replace(@"__POWER_NAME__", power.Name);
+                        macro = macro.Replace(@"__WEAPON_LIST__", weaponList);
+                        macro = macro.Replace(@"__ATTACK_BONUS_LIST__", attackBonusList);
+                        macro = macro.Replace(@"__DAMAGE_LIST__", damageList);
+                        macro = macro.Replace(@"__MAX_DAMAGE_LIST__", maxDamageList);
+                        macro = macro.Replace(@"__MULTIPLE_TARGETS__", "0");
+                        macro = macro.Replace(@"__POWER_CARD__", (power.CompendiumEntry != null) ? power.CompendiumEntry : string.Empty);
+                    }
 
-                    writer.WriteLine("======================================================================");
+                    // encode all special characters as proper url supported characters
+                    string encodedMacro = HttpUtility.UrlEncode(macro);
+
+                    string macroCreation = MacroCreationTemplate;
+                    macroCreation = macroCreation.Replace(@"__MACRO_NAME__", power.Name);
+                    macroCreation = macroCreation.Replace(@"__MACRO_BACKGROUND_COLOR__", "blue");
+                    macroCreation = macroCreation.Replace(@"__MACRO_FOREGROUND_COLOR__", "white");
+                    macroCreation = macroCreation.Replace(@"__MACRO_GROUP__", "At-Will");
+                    macroCreation = macroCreation.Replace(@"__MACRO_CODE__", encodedMacro);
+
+                    writer.WriteLine(macroCreation);
+
+                    // separator for readability
+                    writer.WriteLine(@"<!-- ======================================================================= -->");
                 }
             }
         }
