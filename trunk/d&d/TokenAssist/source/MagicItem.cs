13ebb844ea6d0d5b84b3d63f9afffee3c6084b41
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2009-07-24 06:33:29 +0000

    adding empty and unused magic item class

---
+++ trunk/d&d/TokenAssist/source/MagicItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TokenAssist
+{
+    public class MagicItem
+    {
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        private string mName = string.Empty;
+    }
+}
