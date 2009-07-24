1e7c39191147cce42dd2233ead43995f24668bb5
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2009-07-24 06:33:29 +0000

    adding empty and unused magic item class

--- d&d/TokenAssist/source/Character.cs
+++ d&d/TokenAssist/source/Character.cs
@@ -19,7 +19,14 @@ namespace TokenAssist
             set { mPowers = value; }
         }
 
+        public List<MagicItem> MagicItems
+        {
+            get { return mMagicItems; }
+            set { mMagicItems = value; }
+        }
+
         private string mName = string.Empty;
         private List<Power> mPowers = new List<Power>();
+        private List<MagicItem> mMagicItems = new List<MagicItem>();
     }
 }
