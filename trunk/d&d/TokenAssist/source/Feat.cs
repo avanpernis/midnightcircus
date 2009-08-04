5cb8b95b35a5cb0548784a776e3a9d9a5addcd5d
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2009-08-04 19:43:26 +0000

    oops, forgot this file

---
+++ trunk/d&d/TokenAssist/source/Feat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TokenAssist
+{
+    public class Feat
+    {
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        public string Url
+        {
+            get { return mUrl; }
+            set { mUrl = value; }
+        }
+
+        public string CompendiumEntry
+        {
+            get { return mCompendiumEntry; }
+            set { mCompendiumEntry = value; }
+        }
+
+        public string ShortDescription
+        {
+            get { return mShortDescription; }
+            set { mShortDescription = value; }
+        }
+
+        private string mName = string.Empty;
+        private string mUrl = null;
+        private string mCompendiumEntry = null;
+        private string mShortDescription = null;
+    }
+}
