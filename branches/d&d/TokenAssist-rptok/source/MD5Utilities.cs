1b8c4308e365bc629f9f1f535e737f509ed6d4c2
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-08-22 22:37:14 +0000

    added: MD5Utilities to provide a method to compute file checksums

---
+++ branches/d&d/TokenAssist-rptok/source/MD5Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TokenAssist
+{
+    public static class MD5Utilities
+    {
+        private static MD5 sMD5 = new MD5CryptoServiceProvider();
+
+        /// <summary>
+        /// Compute an MD5 checksum for the specified file
+        /// </summary>
+        /// <param name="filename">The filename whose checksum will be computed</param>
+        /// <returns>the checksum of the specified file</returns>
+        public static string ComputeChecksum(string filename)
+        {           
+            using (FileStream stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in sMD5.ComputeHash(stream))
+                {
+                    builder.Append(b.ToString("x"));
+                }
+
+                return builder.ToString();
+            }
+        }   
+    }
+}
