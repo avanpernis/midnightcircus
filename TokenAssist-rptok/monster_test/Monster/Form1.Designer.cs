f5835c18b8d9c7a0935b966f3df34f1a204f8679
angry.daniel <angry.daniel@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-08-28 22:43:40 +0000

    Monster experiment committed

---
+++ TokenAssist-rptok/monster_test/Monster/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextOutput = new System.Windows.Forms.TextBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextOutput
+            // 
+            this.TextOutput.Location = new System.Drawing.Point(12, 53);
+            this.TextOutput.Multiline = true;
+            this.TextOutput.Name = "TextOutput";
+            this.TextOutput.Size = new System.Drawing.Size(914, 485);
+            this.TextOutput.TabIndex = 0;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(68, 26);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 550);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.TextOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextOutput;
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
+
