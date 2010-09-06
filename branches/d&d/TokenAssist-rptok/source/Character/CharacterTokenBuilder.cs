da9ff39b6badf43d48c851ef6cb3bbc7d6f3a53d
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-06 05:30:53 +0000

    refactored common parts into an actor subclass (monster is an actor now, character still needs heavy refactoring onto the common system)

---
+++ branches/d&d/TokenAssist-rptok/source/Character/CharacterTokenBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TokenAssist
+{
+    public static class CharacterTokenBuilder
+    {
+        public static void WriteToken(Character c, string filename, string tokenImage, string tokenPortrait)
+        {
+            Token t = new Token();
+
+            t.TokenImage = tokenImage;
+            t.TokenPortrait = tokenPortrait;
+            
+            t.Write(filename);
+        }
+    }
+}
