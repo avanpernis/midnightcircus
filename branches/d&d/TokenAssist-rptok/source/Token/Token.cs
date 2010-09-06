b6fa6836157890a78f129f60bbeb7849bb0adfbd
angry.daniel <angry.daniel@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-06 02:28:58 +0000

    Add zip and md5 to WriteToken it still isn't quite useful though

--- branches/d&d/TokenAssist-rptok/source/Token/Token.cs
+++ branches/d&d/TokenAssist-rptok/source/Token/Token.cs
@@ -10,13 +10,65 @@ namespace TokenAssist
     {
         public string Name = "";
         public string PropertyType = "4ePlayer";
+        public string TokenPortrait = null;
+        public string TokenImage = null;
 
         public List<TokenMacro> Macros = new List<TokenMacro>();
         public List<TokenProperty> Properties = new List<TokenProperty>();
-
+        
+        /// <summary>
+        /// Write the given file to a rptok at the given file
+        /// </summary>
+        /// <param name="filename">The full path the rptok file should be created as</param>
         public void Write(string filename)
         {
-            using (StreamWriter file = new StreamWriter(filename))
+            // build a temporary directory we can start creating our files in
+            string tempPath = System.IO.Path.GetTempPath();
+            string tokenPath = System.IO.Path.Combine(tempPath, "TokenAssist");
+
+            if (System.IO.Directory.Exists(tokenPath))
+                System.IO.Directory.Delete(tokenPath, true);
+
+            System.IO.Directory.CreateDirectory(tokenPath);
+
+            string assetpath = System.IO.Path.Combine(tokenPath, "assets");
+            System.IO.Directory.CreateDirectory(assetpath);
+
+            // create the content.xml file
+            string contentPath = System.IO.Path.Combine(tokenPath, "content.xml");
+            try
+            {
+                WriteContentFile(contentPath);
+            }
+            catch (Exception e)
+            {
+                MessageSystem.Error("Error creating content.xml file" + e.Message);
+                throw;
+            }
+
+            // create the properties.xml file
+            string propertiesFilePath = System.IO.Path.Combine(tokenPath, "properties.xml");
+            try
+            {
+                WritePropertiesFile(propertiesFilePath);
+            }
+            catch (Exception e)
+            {
+                MessageSystem.Error("Error creating properties.xml file" + e.Message);
+                throw;
+            }
+
+            ZipUtilities.ZipDirectory(tokenPath, filename);
+        }
+
+
+        /// <summary>
+        /// Create the content.xml file that is part of the token with the given full path filename
+        /// </summary>
+        /// <param name="contentFilename">The full path of the filename to write the new token file to</param>
+        protected void WriteContentFile(string contentFilename)
+        {
+            using (StreamWriter file = new StreamWriter(contentFilename))
             {
                 StringBuilder builder = new StringBuilder();
 
@@ -24,6 +76,44 @@ namespace TokenAssist
 
                 result = result.Replace(@"###TOKEN_NAME###", Name);
                 result = result.Replace(@"###PROP_TYPE###", PropertyType);
+
+                // put in the md5 calculated for the token image
+                string portraitMD5 = "";
+                if (TokenPortrait == null)
+                {
+                    MessageSystem.Warning("No token portrait image specified");
+                }
+                else
+                {
+                    try
+                    {
+                        portraitMD5 = MD5Utilities.ComputeChecksum(TokenPortrait);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageSystem.Warning("Problem loading token image" + e.Message);
+                    }
+                }
+                result = result.Replace(@"###IMAGE_MD5_SUM###", TokenImage);
+
+                // put in the md5 calculated for the portrait image
+                string tokenImageMD5 = "";
+                if (TokenImage == null)
+                {
+                    MessageSystem.Warning("No token image specified");
+                }
+                else
+                {
+                    try
+                    {
+                        tokenImageMD5 = MD5Utilities.ComputeChecksum(TokenImage);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageSystem.Warning("Problem loading portrait image" + e.Message);
+                    }
+                }
+                result = result.Replace(@"###PORTRAIT_MD5###", TokenPortrait);
 
                 // build the property block
                 builder.Clear();
@@ -43,6 +133,18 @@ namespace TokenAssist
                 result = result.Replace(@"###MACRO_SECTION###", builder.ToString());
 
                 file.Write(result);
+            }
+        }
+
+        /// <summary>
+        /// Write the properties.xml to the given path
+        /// </summary>
+        /// <param name="filepath">The full path of the filename to write to</param>
+        protected void WritePropertiesFile(string filepath)
+        {
+            using (StreamWriter file = new StreamWriter(filepath))
+            {
+                file.Write(global::TokenAssist.Properties.Resources.TokenPropertiesFileTemplate);
             }
         }
     }
