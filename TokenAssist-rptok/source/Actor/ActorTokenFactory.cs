ffa9b08a922b118d389d2e672c4c04b930d705c0
brian.cumming <brian.cumming@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-09 05:35:59 +0000

    added: actor token factory to handle common initialization stuff before returning a token to use for derived class stuff

---
+++ TokenAssist-rptok/source/Actor/ActorTokenFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TokenAssist
+{
+    public static class ActorTokenFactory
+    {
+        public static Token Create(Actor actor, string tokenType, string tokenImage, string tokenPortrait)
+        {
+            Token token = new Token();
+
+            token.Name = actor.Name;
+            token.TokenType = tokenType;
+            token.TokenImage = tokenImage;
+            token.TokenPortrait = tokenPortrait;
+
+            token.AddProperty("Level", actor.Level);
+            token.AddProperty("HalfLevel", actor.HalfLevel);
+            token.AddProperty("Speed", actor.Speed);
+            token.AddProperty("ActionPoints", actor.ActionPoints);
+
+            token.AddProperty("CurrentHitPoints", actor.HP);
+            token.AddProperty("MaxHitPoints", actor.HP);
+            token.AddProperty("TempHitPoints", 0);
+
+            token.AddProperty("CurrentHealingSurges", actor.HealingSurges);
+            token.AddProperty("MaxHealingSurges", actor.HealingSurges);
+            token.AddProperty("HealingSurgeValue", actor.HealingSurgeValue);
+
+            foreach (KeyValuePair<string, AbilityScore> pair in actor.Abilities)
+            {
+                token.AddProperty(pair.Key, pair.Value.Value);
+                token.AddProperty(pair.Key + "Modifier", pair.Value.Modifier);
+            }
+
+            foreach (KeyValuePair<string, int?> pair in actor.Defenses)
+            {
+                token.AddProperty(pair.Key, pair.Value);
+            }
+
+            foreach (KeyValuePair<string, int> pair in actor.Skills)
+            {
+                token.AddProperty(pair.Key, pair.Value);
+            }
+
+            return token;
+        }
+    }
+}
--- TokenAssist-rptok/source/Monster/MonsterTokenBuilder.cs
+++ TokenAssist-rptok/source/Monster/MonsterTokenBuilder.cs
@@ -9,43 +9,11 @@ namespace TokenAssist
     {
         public static void WriteToken(Monster monster, string filename, string tokenImage, string tokenPortrait)
         {
-            Token t = new Token();
+            Token token = ActorTokenFactory.Create(monster, "4eMonster", tokenImage, tokenPortrait);
 
-            t.Name = monster.Name;
-            t.TokenType = "4eMonster";
-            t.TokenImage = tokenImage;
-            t.TokenPortrait = tokenPortrait;
+            // TODO custom monster stuff
 
-            t.AddProperty("Level", monster.Level);
-            t.AddProperty("HalfLevel", monster.HalfLevel);
-            t.AddProperty("Speed", monster.Speed);
-            t.AddProperty("ActionPoints", monster.ActionPoints);
-
-            t.AddProperty("CurrentHitPoints", monster.HP);
-            t.AddProperty("MaxHitPoints", monster.HP);
-            t.AddProperty("TempHitPoints", 0);
-
-            t.AddProperty("CurrentHealingSurges", monster.HealingSurges);
-            t.AddProperty("MaxHealingSurges", monster.HealingSurges);
-            t.AddProperty("HealingSurgeValue", monster.HP / 4);
-
-            foreach (KeyValuePair<string, AbilityScore> pair in monster.Abilities)
-            {
-                t.AddProperty(pair.Key, pair.Value.Value);
-                t.AddProperty(pair.Key + "Modifier", pair.Value.Modifier);
-            }
-
-            foreach (KeyValuePair<string, int?> pair in monster.Defenses)
-            {
-                t.AddProperty(pair.Key, pair.Value);
-            }
-
-            foreach (KeyValuePair<string, int> pair in monster.Skills)
-            {
-                t.AddProperty(pair.Key, pair.Value);
-            }
-            
-            t.Write(filename);
+            token.Write(filename);
         }
     }
 }
