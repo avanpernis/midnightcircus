0bbd24dde2bd49bcde623620ad3f5cb9884fc083
angry.daniel@gmail.com <angry.daniel@gmail.com@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-12 19:26:24 +0000

    - sort of working macros for monsters

--- TokenAssist-rptok/source/Monster/MonsterTokenBuilder.cs
+++ TokenAssist-rptok/source/Monster/MonsterTokenBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security;
 
 namespace TokenAssist
 {
@@ -12,21 +13,51 @@ namespace TokenAssist
             Token token = ActorTokenFactory.Create(monster, "4eMonster", tokenImage, tokenPortrait);
 
             // convert monster powers to token powers
+            foreach (MonsterPower p in monster.Powers)
+            {
+                TokenMacro m = MacroFromPower(p);
+                token.AddMacro(m);
+            }
             
             token.Write(filename);
         }
         
         public static TokenMacro MacroFromPower(MonsterPower power)
         {
-            TokenMacro m = new TokenMacro();
+            TokenMacro macro = new TokenMacro();
 
-            StringBuilder command = new StringBuilder();
+            string command = null;
 
-            if (power.AttackBonus != null)
+            macro.Name = SecurityElement.Escape(power.Name);
+            macro.Group = SecurityElement.Escape(power.Category);
+
+            macro.FontColor = ColorType.black;
+            macro.ButtonColor = ColorType.white;
+
+            if ((power.AttackBonus != null) && (power.Damage != null))
             {
+                command = TokenAssist.Properties.Resources.MonsterPowerMacro;
+                command = command.Replace(@"###NAME###", power.Name);
+                command = command.Replace(@"###ATTACK_BONUS###", power.AttackBonus.ToString());
+                command = command.Replace(@"###DAMAGE###", power.Damage);
+                command = command.Replace(@"###MAX_DAMAGE###", RollUtilities.EvaluateMaximum(power.Damage).ToString());
+
+                if (power.MultiTarget == true)
+                    command = command.Replace(@"###MULTIPLE_TARGETS###", "1");
+                else
+                    command = command.Replace(@"###MULTIPLE_TARGETS###", "0");
             }
 
-            return m;
+            // load the description
+            string desc = "";
+            if (power.Description != null)
+                desc = power.Description;
+
+            command = command.Replace(@"###POWER_CARD###", desc);
+
+            macro.Command = SecurityElement.Escape(command);
+
+            return macro;
         }
     }
 }
