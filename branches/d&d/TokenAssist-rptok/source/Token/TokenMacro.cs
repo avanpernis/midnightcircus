06e8b7e8d881c4aa53e82bdc3a1b16804acd1c07
angry.daniel@gmail.com <angry.daniel@gmail.com@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-12 20:13:18 +0000

    - monster macro actually runs - cleanup token interface

--- branches/d&d/TokenAssist-rptok/source/Token/TokenMacro.cs
+++ branches/d&d/TokenAssist-rptok/source/Token/TokenMacro.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security;
 
 namespace TokenAssist
 {
@@ -28,11 +29,11 @@ namespace TokenAssist
             result = result.Replace(@"###BUTTON_COLOR###", ButtonColor.ToString());
             result = result.Replace(@"###FONT_COLOR###", FontColor.ToString());
 
-            result = result.Replace(@"###MACRO_NAME###", Name);
-            result = result.Replace(@"###TOOLTIP###", ToolTip);
-            result = result.Replace(@"###MACRO_GROUP###", Group);
-            result = result.Replace(@"###MACRO_COMMAND###", Command);
-            result = result.Replace(@"###SORTBY###", SortBy);
+            result = result.Replace(@"###MACRO_NAME###", SecurityElement.Escape(Name));
+            result = result.Replace(@"###TOOLTIP###", SecurityElement.Escape(ToolTip));
+            result = result.Replace(@"###MACRO_GROUP###", SecurityElement.Escape(Group));
+            result = result.Replace(@"###MACRO_COMMAND###", SecurityElement.Escape(Command));
+            result = result.Replace(@"###SORTBY###", SecurityElement.Escape(SortBy));
 
             return result;
         }
