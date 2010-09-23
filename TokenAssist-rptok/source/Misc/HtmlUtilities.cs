ab0ed2144367356a73973ebeb205d4d3e6f3f840
angry.daniel@gmail.com <angry.daniel@gmail.com@b899e4ce-69f1-11de-9529-870f7d8c7d16>
2010-09-23 04:39:15 +0000

    - add color coding for monster powers - fix various bad character issues

--- TokenAssist-rptok/source/Misc/HtmlUtilities.cs
+++ TokenAssist-rptok/source/Misc/HtmlUtilities.cs
@@ -13,5 +13,27 @@ namespace TokenAssist
         {
             return Tag(input, "b");
         }
+
+        public static string ScrubString(string input)
+        {
+            string result = input;
+            // for some reason, compendium uses 3 ?'s for an apostrophe
+            result = result.Replace(@"???", @"'");
+
+            // sometimes there are funky unicode apostrophes as well
+            result = result.Replace("\u2019", @"'");
+
+            // sometimes there are strange unicode spaces
+            result = result.Replace("\uF020", @" ");
+
+            // sometimes there are weird non-hyphen characters used for negative modifiers/penalties
+            result = result.Replace("\u2013", @"-");
+
+            // sometimes there are weird quotation mark characters that do not play nicely
+            result = result.Replace("\u201c", @"'");
+            result = result.Replace("\u201d", @"'");
+
+            return result;
+        }
     }
 }
